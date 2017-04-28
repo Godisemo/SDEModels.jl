fd4e0488fc903c32855022e26b4a4e1068a09fc1
Godisemo <carl.akerlindh@gmail.com>
2017-04-28 18:40:26 +0200

    Add tests for trivial diffusion model

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,10 +17,13 @@ end
     dZ = X*e*dt + f*dW3 -   dW4
 end
 
+@sde_model Wiener dX = dW
 @sde_model Deterministic dX = X*dt
+
 m1 = BlackScholes(0.01, 0.3)
 m2 = Heston(0.01, 10.0, 0.3, 0.1, 0.4)
 m3 = TestModel(1, 2, 3, 4, 5, 6)
+wiener = Wiener()
 deterministic = Deterministic()
 
 @testset "model creation" begin
@@ -30,6 +33,7 @@ deterministic = Deterministic()
   @test dim(m1) == (1,1)
   @test dim(m2) == (2,2)
   @test dim(m3) == (3,4)
+  @test dim(wiener) == (1,1)
   @test dim(deterministic) == (1,0)
 end
 
@@ -37,6 +41,7 @@ end
   @test drift(m1, 100.0) ≈ 100.0 * 0.01
   @test drift(m2, [100.0, 0.4]) ≈ [0.01*100.0, 10.0*(0.3-0.4)]
   @test drift(m3, [0.1, 0.2, 0.3]) ≈ [0.2*1, 0.3*2, 0.1*3]
+  @test drift(wiener, 100.0) ≈ 0.0
   @test drift(deterministic, 100.0) ≈ 100.0
 end
 
@@ -44,6 +49,7 @@ end
   @test diffusion(m1, 100.0) ≈ 100.0 * 0.3
   @test diffusion(m2, [100.0, 0.4]) ≈ [100.0*sqrt(0.4) 0.0; 0.4*0.1*sqrt(0.4) sqrt(1-0.4^2)*0.1*sqrt(0.4)]
   @test diffusion(m3, [0.1, 0.2, 0.3]) ≈ [4 5 0 0; 0 1 1 0; 0 0 6 -1]
+  @test diffusion(wiener, 100.0) ≈ 1.0
   @test diffusion(deterministic, 100.0) ≈ 0.0
 end
 
