68faa3dd423e2ac7f17e4e4bb5fa6610c8d3569c
Godisemo <carl.akerlindh@gmail.com>
2017-05-03 02:18:20 +0200

    Rewrite multilevel sampling again to increase performance

--- src/multilevel.jl
+++ src/multilevel.jl
@@ -1,34 +1,27 @@
-immutable MultilevelState{D,S,T} <: AbstractState{D,S,T}
+type MultilevelState{T<:AbstractState}
   xc::T
   xf::T
-  t::Float64
 end
 
-MultilevelState{T<:Number}(xc::T, xf::T, t::Float64) =
-  MultilevelState{1,T,T}(xc, xf, t)
-MultilevelState{D,T}(xc::SVector{D,T}, xf::SVector{D,T}, t::Float64) =
-  MultilevelState{D,T,SVector{D,T}}(xc, xf, t)
-MultilevelState(xc::AbstractVector, xf::AbstractVector, t::Float64) =
-  MultilevelState(convert(SVector{length(xc)}, xc), convert(SVector{length(xf)}, xf), t)
-
-coarse(state::MultilevelState) = TimeDependentState(state.xc, state.t)
-fine(state::MultilevelState) = TimeDependentState(state.xf, state.t)
+coarse(state::MultilevelState) = state.xc
+fine(state::MultilevelState) = state.xf
 
 coarse(A::AbstractArray) = reshape([ coarse(x) for x in A ], size(A))
 fine(A::AbstractArray) = reshape([ fine(x) for x in A ], size(A))
 
-multilevel(xc, xf, t) = MultilevelState(xc, xf, t)
-multilevel(x, t) = MultilevelState(x, x, t)
+multilevel(xc, xf) = MultilevelState(xc, xf)
+multilevel(x) = MultilevelState(x, x)
+
+Base.copy(s::MultilevelState) = MultilevelState(s.xc, s.xf)
 
 function sample(model, scheme, state0::MultilevelState; nsubsteps=2)
   subscheme = subdivide(scheme, nsubsteps)
-  prevstate_coarse = coarse(state0)
-  prevstate_fine = fine(state0)
+  prevstate = copy(state0)
   Δw = 0.0
   for j in 1:nsubsteps
     Δw += δw = wiener(model, subscheme)
-    prevstate_fine = step(model, subscheme, prevstate_fine, δw)
+    prevstate.xf = step(model, subscheme, prevstate.xf, δw)
   end
-  prevstate_coarse = step(model, scheme, prevstate_coarse, Δw)
-  MultilevelState(statevalue(prevstate_coarse), statevalue(prevstate_fine), statetime(prevstate_coarse))
+  prevstate.xc = step(model, scheme, prevstate.xc, Δw)
+  prevstate
 end
