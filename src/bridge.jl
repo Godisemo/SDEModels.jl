34689ef25b39ac97513ba064c3ef0e64ee4be13c
Godisemo <carl.akerlindh@gmail.com>
2017-05-04 15:42:56 +0200

    Update bridge sampling

--- src/bridge.jl
+++ src/bridge.jl
@@ -1,18 +1,17 @@
-immutable ModifiedBridge{D,T,S} <: AbstractScheme
+immutable ModifiedBridge{T} <: AbstractScheme
   Δt::Float64
   t1::Float64
-  state1::SDEState{D,T,S}
+  s1::T
 end
 
-subdivide(scheme::ModifiedBridge, nsubsteps) = ModifiedBridge(scheme.Δt / nsubsteps, scheme.state1)
+subdivide(scheme::ModifiedBridge, nsubsteps) = ModifiedBridge(scheme.Δt / nsubsteps, scheme.t1, scheme.s1)
 
-function step(model, scheme::ModifiedBridge, t, state0::SDEState, Δw)
-  x0 = statevalue(state0)
-  x1 = statevalue(scheme.state1)
+function step(model, scheme::ModifiedBridge, t0, s0::SDEState, Δw)
+  x0 = statevalue(s0)
+  x1 = statevalue(scheme.s1)
   t1 = scheme.t1
-  # TODO numerical issues when subdividing
-  μ = (x1 - x0) / (t1 - t)
-  σ = sqrt((t1 - (t + scheme.Δt)) / (t1 - t)) * diffusion(model, state0)
+  μ = (x1 - x0) / (t1 - t0)
+  σ = sqrt((t1 - (t0 + scheme.Δt)) / (t1 - t0)) * diffusion(model, t0, s0)
   x = x0 + μ * scheme.Δt + σ * Δw
   SDEState(x)
 end
