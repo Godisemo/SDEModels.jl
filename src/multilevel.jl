836124d423c32c2c8652710178af4116d10c1a70
Godisemo <carl.akerlindh@gmail.com>
2017-05-04 16:14:06 +0200

    Update multilevel sampling to the new api

--- src/multilevel.jl
+++ src/multilevel.jl
@@ -1,10 +1,10 @@
 immutable MultilevelState{M,T}
-  xc::T
-  xf::T
+  sc::T
+  sf::T
 end
 
-coarse(state::MultilevelState) = state.xc
-fine(state::MultilevelState) = state.xf
+coarse(state::MultilevelState) = state.sc
+fine(state::MultilevelState) = state.sf
 
 coarse(A::AbstractArray) = reshape([ coarse(x) for x in A ], size(A))
 fine(A::AbstractArray) = reshape([ fine(x) for x in A ], size(A))
@@ -12,15 +12,16 @@ fine(A::AbstractArray) = reshape([ fine(x) for x in A ], size(A))
 multilevel{T}(M, xc::T, xf::T) = MultilevelState{M,T}(xc, xf)
 multilevel{T}(M, x::T) = MultilevelState{M,T}(x, x)
 
-function sample{M,T}(model, scheme, state0::MultilevelState{M,T})
+function sample{M,T}(model, scheme, t0, s0::MultilevelState{M,T})
   subscheme = subdivide(scheme, M)
-  prevstate_coarse = coarse(state0)
-  prevstate_fine = fine(state0)
+  scprev = coarse(s0)
+  sfprev = fine(s0)
   Δw = zero(wiener_type(model))
-  for j in 1:M
+  for i in 1:M
+    ti = t0 + (i - 1) * subscheme.Δt
     Δw += δw = wiener(model, subscheme)
-    prevstate_fine = step(model, subscheme, prevstate_fine, δw)
+    sfprev = step(model, subscheme, ti, sfprev, δw)
   end
-  prevstate_coarse = step(model, scheme, prevstate_coarse, Δw)
-  MultilevelState{M,T}(prevstate_coarse, prevstate_fine)
+  scprev = step(model, scheme, t0, scprev, Δw)
+  MultilevelState{M,T}(scprev, sfprev)
 end
