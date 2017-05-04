9c38a0aa3d05e493226012be8a1f2b923e9c1e18
Godisemo <carl.akerlindh@gmail.com>
2017-05-04 23:00:06 +0200

    Add bridge conditional transition densities

--- src/bridge.jl
+++ src/bridge.jl
@@ -15,3 +15,25 @@ function step(model, scheme::ModifiedBridge, t0, s0::SDEState, Δw)
   x = x0 + μ * scheme.Δt + σ * Δw
   SDEState(x)
 end
+
+function _bridge_transition_params(model, scheme, t0, s0, s1)
+  x0 = statevalue(s0)
+  xt = statevalue(s1)
+  x1 = statevalue(scheme.s1)
+  t1 = scheme.t1
+  μ = x0 + (x1 - x0) / (t1 - t0) * scheme.Δt
+  σ = diffusion(model, t0, s0)
+  Σ = (t1 - (t0 + scheme.Δt)) / (t1 - t0) * scheme.Δt * σ * σ'
+  z = xt - μ
+  z, Σ
+end
+
+function transition{D}(model::AbstractSDE{D}, scheme::ModifiedBridge, t0, s0, s1)
+  z, Σ = _bridge_transition_params(model, scheme, t0, s0, s1)
+  1.0 / sqrt(det(2pi*Σ)) * exp(-0.5*dot(z, Σ\z))
+end
+
+function logtransition{D}(model::AbstractSDE{D}, scheme::ModifiedBridge, t0, s0, s1)
+  z, Σ = _bridge_transition_params(model, scheme, t0, s0, s1)
+  -0.5*(D*log(2pi) + log(det(Σ)) + dot(z, Σ\z))
+end
