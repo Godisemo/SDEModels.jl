d011321e09741fa94ca92fbbc776e7e6cbfe9632
Godisemo <carl.akerlindh@gmail.com>
2017-05-22 13:42:29 +0200

    Remove unused code

--- src/schemes/schemes.jl
+++ src/schemes/schemes.jl
@@ -21,7 +21,7 @@ end
 @unconditional_scheme Exact
 @unconditional_scheme EulerMaruyama
 @unconditional_scheme Milstein
-@conditional_scheme ModifiedBridge
+@conditional_scheme   ModifiedBridge
 @unconditional_scheme EulerExponential1
 @unconditional_scheme EulerExponential2
 @unconditional_scheme EulerExponential3
@@ -33,14 +33,6 @@ include("euler_exponential.jl")
 
 subdivide{T<:ConditionalScheme}(scheme::T, nsubsteps) = T(scheme.Δt / nsubsteps, scheme.t, scheme.s)
 subdivide{T<:UnconditionalScheme}(scheme::T, nsubsteps) = T(scheme.Δt / nsubsteps)
-
-wiener_type(::AbstractSDE{0}) = Float64
-wiener_type(::AbstractSDE{1}) = Float64
-wiener_type{D}(::AbstractSDE{D}) = SVector{D,Float64}
-
-wiener{D}(::AbstractSDE{D,0}, scheme::AbstractScheme) = 0.0
-wiener{D}(::AbstractSDE{D,1}, scheme::AbstractScheme) = sqrt(scheme.Δt) * randn()
-wiener{D,M}(::AbstractSDE{D,M}, scheme::AbstractScheme) = sqrt(scheme.Δt) * randn(SVector{M})
 
 function transition(model, scheme, times, data)
   [transition(model, scheme, times[i-1], data[i-1], data[i]) for i in 2:length(data)]
