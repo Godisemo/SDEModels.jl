7b76da36357fa7a0f13c42fb4d42c892b4fedf87
Godisemo <carl.akerlindh@gmail.com>
2019-08-02 22:18:03 +0200

    Fix issue with Milstein and Runge-Kutta for D-dimensional models

--- src/schemes/milstein.jl
+++ src/schemes/milstein.jl
@@ -10,3 +10,11 @@ function step(model::AbstractSDE{1,1}, scheme::Milstein, t0, x0, Δw)
   x = x0 + μ * scheme.Δt + σ * Δw + σ * ∂σ * (Δw^2 - scheme.Δt) / 2
   x
 end
+
+function step(model::AbstractSDE{D,1}, scheme::Milstein, t0, x0, Δw) where D
+  μ = drift(model, t0, x0)
+  σ = diffusion(model, t0, x0)
+  ∂σ = ForwardDiff.jacobian(x->diffusion(model, t0, x), x0)
+  x = x0 + μ * scheme.Δt + σ * Δw + ∂σ * σ * (Δw.^2 - scheme.Δt) / 2
+  x
+end
--- src/schemes/runge_kutta.jl
+++ src/schemes/runge_kutta.jl
@@ -1,8 +1,11 @@
+_vec_or_nothing(x::Number) = x
+_vec_or_nothing(x::AbstractArray) = vec(x)
+
 function step(model::AbstractSDE{D,1}, scheme::RungeKutta, t0, x0, Δw) where D
   μ = drift(model, t0, x0)
   σ = diffusion(model, t0, x0)
-  x0hat = x0 + μ * scheme.Δt + σ * sqrt(scheme.Δt)
+  x0hat = x0 + μ * scheme.Δt + _vec_or_nothing(σ) * sqrt(scheme.Δt)
   σpred = diffusion(model, t0, x0hat)
-  x = x0 + μ * scheme.Δt + σ * Δw + (σpred - σ) * (Δw^2 - scheme.Δt) / (2 * sqrt(scheme.Δt))
+  x = x0 + μ * scheme.Δt + σ * Δw + (σpred - σ) * (Δw.^2 - scheme.Δt) / (2 * sqrt(scheme.Δt))
   x
 end
